88f4516c5cf343bbf6b5150e7aade19fc561ed7f
lakshmify <nellore.lakshmik@gmail.com>
2025-10-25 02:20:55 +0000

    streamlit app to display df

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pandas as pd
+
+data= {
+"id":range(1,11),
+"name" : [f"Item_{i}" for i in range(1,11)],
+"value" : [i*10 for i in range(1,11)]
+}
+
+df=pd.DataFrame(data)
+
+st.title("Hello World !")
+st.dataframe(df)
